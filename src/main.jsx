05d7dcbce04a8ffbf16e3e81b56498fc3e2fc99c
Auratha <auratha1000@gmail.com>
2024-10-09 20:38:26 +0630

    fix github 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+const currentUrl = window.location.href;
+
+if (currentUrl !== window.location.origin + "/") {
+  window.location.replace("/index.html");
+}
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
