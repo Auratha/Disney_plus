9bcc683016b6ad20620129bf742d9eeef697847c
Auratha <auratha1000@gmail.com>
2024-10-09 20:44:11 +0630

    nothing

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
-const currentUrl = window.location.href;
-
-if (currentUrl !== window.location.origin + "/") {
-  window.location.replace("/index.html");
-}
-
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
